090fbcea71350b16bee344e2f086e5c5c84e5f15
Jacob Michels <jacob.michels2025@gmail.com>
2021-10-28 01:24:29 -0400

    add comments and rename data access class to repository

--- course-sense-dotnet/Controllers/NotificationRequestController.cs
+++ course-sense-dotnet/Controllers/NotificationRequestController.cs
@@ -1,7 +1,7 @@
-﻿using course_sense_dotnet.Models;
+﻿using course_sense_dotnet.Application.WebAdvisor.RequestManager;
+using course_sense_dotnet.Models;
 using course_sense_dotnet.Repository;
 using course_sense_dotnet.Validators;
-using course_sense_dotnet.WebAdvisor.RequestManager;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -13,6 +13,7 @@ using System.Threading.Tasks;
 
 namespace course_sense_dotnet.Controllers
 {
+    // This class is a Controller for the route /NotificationRequest.
     [Route("[controller]")]
     [ApiController]
     public class NotificationRequestController : ControllerBase
@@ -21,46 +22,53 @@ namespace course_sense_dotnet.Controllers
         private readonly IRequestManager requests;
         private readonly IContactValidator contactValidation;
         private readonly SynchronizedCollection<NotificationRequest> notificationRequests;
-        private readonly IDBRepository dataAccess;
+        private readonly IDBRepository repository;
+
         public NotificationRequestController(ILogger<NotificationRequestController> logger,
             IRequestManager requests,
             SynchronizedCollection<NotificationRequest> notificationRequests,
             IContactValidator contactValidation,
-            IDBRepository dataAccess)
+            IDBRepository repository)
         {
             this.logger = logger;
             this.requests = requests;
             this.notificationRequests = notificationRequests;
             this.contactValidation = contactValidation;
-            this.dataAccess = dataAccess;
+            this.repository = repository;
         }
+
+        // This method handles HTTP POST requests to the route /NotificationRequest.
         [HttpPost]
         public async Task<IActionResult> RequestCourseNotification(NotificationRequest requestData)
         {
+            // Perform validation on the course and supplied contact info, logging any errors.
             try
             {
                 if (!await requests.CheckCourseExists(requestData.RequestedCourse))
                 {
-                    return UnprocessableEntity("Bad course");
+                    return BadRequest("Bad course");
                 }
                 if (!contactValidation.ValidateContactInfo(requestData.Phone, requestData.Email))
                 {
-                    return UnprocessableEntity("Bad contact");
+                    return BadRequest("Bad contact");
                 }
                 
             }
             catch(Exception e)
             {
                 logger.LogError($"Error in {nameof(NotificationRequestController)}: {e.Message} | Status 500 returned");
-                return StatusCode(500,"Error during validation");
+                return StatusCode(500,"Server error during validation");
             }
-            if (!dataAccess.InsertRequest(requestData))
+
+            // If the request passes validation, add it to the repository and to the in-memory collection.
+            if (!repository.AddRequest(requestData))
             {
                 logger.LogError($"Error in {nameof(NotificationRequestController)} DB Insert failed, Status 500 returned");
-                return StatusCode(500,"Failed DB insert");
+                return StatusCode(500,"Server error: failed DB insert");
             }
-            logger.LogInformation($"New request added: {requestData.Email}|{requestData.Phone}|{requestData.RequestedCourse.Term}|{requestData.RequestedCourse.Subject}|{requestData.RequestedCourse.Code}|{requestData.RequestedCourse.Section}");
             notificationRequests.Add(requestData);
+            logger.LogInformation($"New request added: {requestData.Email}|{requestData.Phone}|{requestData.RequestedCourse.Term}|{requestData.RequestedCourse.Subject}|{requestData.RequestedCourse.Code}|{requestData.RequestedCourse.Section}");
+            
             return Ok();
         }
     }
